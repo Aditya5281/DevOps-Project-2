344bc2c6ca9ae3930deb1fda98aaa7028666da1f
= <=>
2025-09-11 12:33:41 +0530

    Updated

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -3,6 +3,6 @@ data "azurerm_resource_group" "rgname" {
 }
 
 data "azurerm_user_assigned_identity" "useridentity" {
-  name = "mi-use-iac1-dev"
+  name = "mi-use-iact1-dev"
   resource_group_name = "RGP-USE-IACT1-DEV"
-}
+} 
